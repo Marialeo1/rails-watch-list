b8c0ac3b4eb14ebb2e8df2268dc9b619fccd37dd
Maria Varela <leonor.varela91330@gmail.com>
2022-04-23 12:40:19 +0200

    model & validation

---
+++ db/migrate/20220423093052_create_titles.rb
@@ -0,0 +1,12 @@
+class CreateTitles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.text :overview
+      t.string :post_url
+      t.float :rating
+
+      t.timestamps
+    end
+  end
+end
